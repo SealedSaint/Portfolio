daef275d16e5ba2bf05a40159df0d5c3340a8a52
David Clark <davidclark314@gmail.com>
2017-04-07 13:56:09 -0600

    Updating bundle for changes.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import * as types from './types'
 
 export function changeTab(tabVal) {
+	console.log('changing tab and scrolling to top')
 	window.scrollTo(0, 0)
 	return {
 		type: types.CHANGE_TAB,
