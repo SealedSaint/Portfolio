608c0596c1ccdef6ac3d6fbdf0a3c3737bc96079
David Clark <davidclark314@gmail.com>
2017-04-07 13:52:47 -0600

    Updating bundle for changes.

--- bundle.js
+++ bundle.js
@@ -14690,6 +14690,7 @@ var types = _interopRequireWildcard(_types);
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function changeTab(tabVal) {
+	window.scrollTo(0, 0);
 	return {
 		type: types.CHANGE_TAB,
 		tabVal: tabVal
